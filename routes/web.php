26b778c598c50412298fb759f384754a08a02eca
William Garcia <wgarcia@tradercomusa.com>
2023-07-15 20:29:10 -0400

    Elimina comentario en la ruta.

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,5 @@ use App\Http\Controllers\PageController;
 
 
 Route::get('/',            [PageController::class, 'blog'])->name('blog');
-#Route::get('/{post:slug}', [PageController::class, 'post'])->name('post');
-Route::get('/post/{slug}', [PageController::class, 'post'])->name('post');
+Route::get('/{post:slug}', [PageController::class, 'post'])->name('post');
+
